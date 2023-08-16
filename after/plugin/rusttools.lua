a1a03c6c3d4dd3a9f8a80f04e244b78b2c2f085b
Zion Klinger <zion.klinger1@gmail.com>
2023-08-15 23:18:30 -0400

    changed rust tools setup

--- after/plugin/rusttools.lua
+++ after/plugin/rusttools.lua
@@ -14,11 +14,10 @@ rt.setup({
         on_attach = function(c, bufnr)
             local opts = { buffer = bufnr, remap = false }
             -- RUST SPECIFIC
-            vim.keymap.set("n", "<C-h>", rt.hover_actions.hover_actions, { buffer = bufnr })
             -- Code action groups
             vim.keymap.set("n", "<Leader>r", rt.code_action_group.code_action_group, { buffer = bufnr })
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+            vim.keymap.set("n", "K", function() rt.hover_actions.hover_actions() end, opts)
             vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
             vim.keymap.set("n", "<leader>d", function() vim.diagnostic.goto_next() end, opts)
             vim.keymap.set("n", "<leader>q", function() vim.diagnostic.goto_prev() end, opts)
